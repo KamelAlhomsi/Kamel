e3743ec06e5c9569e9529d2e74fe94d9080f6952
Kamel Alhomsi <KamelAlhomsi@users.noreply.github.com>
2018-04-30 18:02:34 +0200

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+	var copy = document.getElementById("credit");
+	if (copy == null) {
+		window.location.href = "https://www.alhomsi4elc.de/"
+	};
+	copy.setAttribute("href", "https://www.alhomsi4elc.de/");
+	copy.innerHTML = "المهندس كامل الحمصي"
+};
